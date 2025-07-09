2d709238604322806f68c066903a16dc4b36aa31
Mete Aslan <105922892+MeteAslan32@users.noreply.github.com>
2025-07-09 13:38:53 +0300

    Base functions

--- src/gipGauge.cpp
+++ src/gipGauge.cpp
@@ -15,3 +15,30 @@ gipGauge::gipGauge() {
 gipGauge::~gipGauge() {
 }
 
+void gipGauge::setup() {
+	value = 0;
+	min = 0;
+	max = 100;
+}
+
+void gipGauge::update() {
+}
+
+void gipGauge::draw() {
+}
+
+void gipGauge::setMin(int min) {
+	this->min = min;
+}
+
+void gipGauge::setMax(int max) {
+	this->max = max;
+}
+
+void gipGauge::setValue(int value) {
+	this->value = value;
+}
+
+int gipGauge::getValue() {
+	return this->value;
+}
--- src/gipGauge.h
+++ src/gipGauge.h
@@ -16,6 +16,20 @@ class gipGauge : public gBasePlugin{
 public:
 	gipGauge();
 	virtual ~gipGauge();
+
+	void setup() override;
+	void update() override;
+	void draw();
+
+	void setMin(int min);
+	void setMax(int max);
+	void setValue(int value);
+	int getValue();
+
+private:
+	int min;
+	int max;
+	int value;
 };
 
 #endif /* SRC_GIPGAUGE_H_ */
